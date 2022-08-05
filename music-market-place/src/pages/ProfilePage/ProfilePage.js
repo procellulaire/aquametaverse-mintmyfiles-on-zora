7adc201d186bd646d5be769c34cade635d2ab3e4
Atolagbe David Tolulope <atolagbedt@gmail.com>
2022-08-05 23:12:20 +0100

    fatch nfts with zora

--- music-market-place/src/pages/ProfilePage/ProfilePage.js
+++ music-market-place/src/pages/ProfilePage/ProfilePage.js
@@ -1,7 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useQuery } from 'react-query';
+// import { request, gql } from 'graphql-request';
+// import { NFTPreview } from "@zoralabs/nft-components";
+
+
+const endpoint = 'https://api.zora.co/graphql';
+
+const collectionNFT = gql`
+  {
+    tokens(
+      networks: [{ network: ETHEREUM, chain: MAINNET }]
+      pagination: { limit: 3 }
+      where: { ownerAddresses: "jacob.eth" }
+    ) {
+      nodes {
+        token {
+          collectionAddress
+          tokenId
+          name
+          owner
+          image {
+            url
+          }
+          metadata
+        }
+      }
+    }
+  }
+`;
 
 export default function ProfilePage() {
+  const [data, setData] = useState([])
+
+
+  React.useEffect(() => {
+    fetch('https://api.zora.co/graphql', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ query: collectionNFT }),
+    })
+      .then((response) => response.json())
+      .then((data) => {setData(data.data.tokens.nodes)
+      console.log(data.data.tokens.nodes)});
+  }, []);
+
+
+
   return (
-    <div>ProfilePage</div>
-  )
+    <div>
+      <h1>Profile Page </h1>
+      <ul>
+        {data.map((launch) => (
+          <li key={launch.tokenId}>{launch.token.metadata.name}</li>
+        //   <NFTPreview
+        //   contract={asksNFT.contractAddress}
+        //   id={asksNFT.tokenId}
+        //   showBids={false}
+        //   showPerpetual={false}
+        // />
+        ))}
+      </ul>
+    </div>
+  );
 }
