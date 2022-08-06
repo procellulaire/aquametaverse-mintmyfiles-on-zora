2f0656ac308be87957ff5114f72c76aa50d16357
FaBbiye <frnz.bagheri@gmail.com>
2022-08-06 07:49:54 +0400

    mint to ipfs
    an error on fetching metadata from ipfs

--- music-market-place/src/components/Minter/UploadFile.js
+++ music-market-place/src/components/Minter/UploadFile.js
@@ -4,15 +4,13 @@ import { Alert, Button, Container, Form, Spinner } from "react-bootstrap";
 import { NFTContext } from "../../context/NFTContext";
 import { StepContext } from "../../context/StepContext";
 import { UserContext } from "../../context/UserContext";
-import { saveFileToMintFiles } from "../../helpers/FileData";
-import { useMoralisQuery } from "react-moralis";
 
 export default function UploadFile() {
   const [alert, setAlert] = useState({});
   const [showAlert, setShowAlert] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [file, setFile] = useState();
-  const [myFiles, setMyFiles] = useState([]);
+  const [cover, setCover] = useState();
   const { isAuth, wallet } = useContext(UserContext);
   const { nft, setNft } = useContext(NFTContext);
   const { step, setStep } = useContext(StepContext);
@@ -22,56 +20,17 @@ export default function UploadFile() {
     console.log("file", e.target.files[0]);
   };
 
-  // const { fetch } = useMoralisQuery(
-  //   "FilesMinted",
-  //   (query) => query.equalTo("minter_address", wallet),
-  //   [],
-  //   { autoFetch: false }
-  // );
-  // const getFiles = (fileData) => {
-  //   fetch({
-  //     onSuccess: (data) => {
-  //       console.log("myFiles", data);
-
-  //       if (data.length > 0) {
-  //         const temp = data.filter((d) => {
-  //           d.get("fileName") === fileData.name &&
-  //             d.get("fileSize") === fileData.size;
-  //         });
-  //         console.log("temp", temp);
-  //         setMyFiles(temp);
-  //       }
-  //     },
-  //     onError: (error) => {
-  //       console.error(error);
-  //     },
-  //   });
-  // };
+  const handleOnChangeCover = (e) => {
+    setCover(e.target.files[0]);
+    console.log("cover", e.target.files[0]);
+  };
 
   const mintFile = async () => {
     setIsLoading(true);
     const client = new NFTStorage({ token: process.env.REACT_APP_NFT_STORAGE });
     if (isAuth) {
-      //check whether the file uploaded prev
-      // getFiles(file);
-      // if (myFiles.length > 0) {
-      //   setAlert({
-      //     type: "danger",
-      //     message: `It seems that you already uploaded this file.`,
-      //     title: "Duplicate upload!",
-      //   });
-      // }
-      const cid = await client.storeBlob(file);
-      console.log("cid", cid);
-      setNft({ ...nft, CID: cid });
-      saveFileToMintFiles(file, wallet, cid);
-
+      setNft({ ...nft, file: file, cover:cover });      
       setIsLoading(false);
-      setAlert({
-        type: "success",
-        message: `Your file has been uploaded to IPFS successfully. CID: ${cid}`,
-        title: "Upload Successfully!",
-      });
       setStep(1);
     } else {
       setAlert({
@@ -104,8 +63,12 @@ export default function UploadFile() {
           <Form.Label>Choose your File</Form.Label>
           <Form.Control type="file" onChange={handleOnChangeFile} />
         </Form.Group>
+        <Form.Group controlId="formFile" className="mb-3">
+          <Form.Label>Choose your Cover Image</Form.Label>
+          <Form.Control type="file" onChange={handleOnChangeCover} />
+        </Form.Group>
         <Button color="primary" onClick={mintFile} disabled={isLoading}>
-          {isLoading ? <Spinner animation="border" /> : "Mint Your File"}
+          {isLoading ? <Spinner animation="border" /> : "Next Step"}
         </Button>
       </Form>
     </Container>
